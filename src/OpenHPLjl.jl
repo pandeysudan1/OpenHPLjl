24bfdf0ce30bbc71e7753f755255ffc284fc8dbb
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-15 16:36:08 +0200

    Export droop governor

--- src/OpenHPLjl.jl
+++ src/OpenHPLjl.jl
@@ -15,6 +15,6 @@ export t, D
 export Contact, connect_hydraulic
 export darcy_factor, darcy_friction
 export Reservoir, HydroPipe, SurgeTank, PressureBoundary
-export Turbine, SimpleGenerator
+export Turbine, SimpleGenerator, DroopGovernor
 
 end # module
