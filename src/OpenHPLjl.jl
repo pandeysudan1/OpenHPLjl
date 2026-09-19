eefcf22bc8342b8f5feb6e65032dba2d6e0afa43
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 22:33:14 +0200

    Wire hydraulic connector and reservoir components

--- src/OpenHPLjl.jl
+++ src/OpenHPLjl.jl
@@ -1,7 +1,14 @@
 module OpenHPLjl
 
 using ModelingToolkit
+using ModelingToolkit: t_nounits as t, D_nounits as D
 
+include("Interfaces/HydraulicPort.jl")
+include("Reservoirs/InfiniteReservoir.jl")
+include("Reservoirs/Reservoir.jl")
+
+export HydraulicPort
+export InfiniteReservoir, Reservoir
 export project_status
 
 """
@@ -9,6 +16,6 @@ export project_status
 
 Return the current project phase.
 """
-project_status() = "OpenHPLjl kickoff: equation-based hydropower modeling with ModelingToolkit.jl"
+project_status() = "OpenHPLjl reservoir milestone: hydraulic connector and first reservoir models"
 
 end
