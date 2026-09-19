853bf02c247f8b1185fccb1f0babe87452ee0358
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-20 00:21:54 +0200

    Add SixthOrderGenerator source specification

---
+++ src/Electrical/Generators/SixthOrderGenerator.jl
@@ -0,0 +1,14 @@
+struct SixthOrderGeneratorSpec
+    H::Float64
+    D::Float64
+    Xd::Float64
+    Xq::Float64
+    Xd_p::Float64
+    Xq_p::Float64
+    Xd_pp::Float64
+    Xq_pp::Float64
+    Tdo_p::Float64
+    Tqo_p::Float64
+    Tdo_pp::Float64
+    Tqo_pp::Float64
+end
