5c60972ac5886885f840c2d9369c903d4569c067
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 22:40:02 +0200

    Wire RigidPipe into package source

--- src/OpenHPLjl.jl
+++ src/OpenHPLjl.jl
@@ -6,9 +6,11 @@ using ModelingToolkit: t_nounits as t, D_nounits as D
 include("Interfaces/HydraulicPort.jl")
 include("Reservoirs/InfiniteReservoir.jl")
 include("Reservoirs/Reservoir.jl")
+include("Waterways/RigidPipe.jl")
 
 export HydraulicPort
 export InfiniteReservoir, Reservoir
+export RigidPipe
 export project_status
 
 """
@@ -16,6 +18,6 @@ export project_status
 
 Return the current project phase.
 """
-project_status() = "OpenHPLjl reservoir milestone: hydraulic connector and first reservoir models"
+project_status() = "OpenHPLjl rigid-pipe milestone: reservoir storage plus waterway momentum"
 
 end
