819642ece369388e0682363c1f66fc412020282c
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 22:57:03 +0200

    Wire turbine family and reorganized waterway paths

--- src/OpenHPLjl.jl
+++ src/OpenHPLjl.jl
@@ -6,12 +6,15 @@ using ModelingToolkit: t_nounits as t, D_nounits as D
 include("Interfaces/HydraulicPort.jl")
 include("Reservoirs/InfiniteReservoir.jl")
 include("Reservoirs/Reservoir.jl")
-include("Waterways/RigidPipe.jl")
-include("Waterways/SurgeTank.jl")
+include("Waterways/RigidPipes/RigidPipe.jl")
+include("Waterways/SurgeTanks/SurgeTank.jl")
+include("Turbines/IdealTurbine.jl")
+include("Turbines/SimpleGateTurbine.jl")
 
 export HydraulicPort
 export InfiniteReservoir, Reservoir
 export RigidPipe, SurgeTank
+export IdealTurbine, SimpleGateTurbine
 export project_status
 
 """
@@ -19,6 +22,6 @@ export project_status
 
 Return the current project phase.
 """
-project_status() = "OpenHPLjl waterway milestone: reservoir, rigid pipe, and surge tank"
+project_status() = "OpenHPLjl turbine milestone: modular waterways and first turbine model family"
 
 end
