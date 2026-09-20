5c100876d4521b9d5684d017952c8984cbf7ddf4
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-20 09:35:51 +0200

    Add SMIB active-power disturbance parameter

--- src/Electrical/Grids/SMIBNetwork.jl
+++ src/Electrical/Grids/SMIBNetwork.jl
@@ -1,5 +1,5 @@
 """
-    SMIBNetwork(; name, Pmax=1.0e6, delta_grid=0.0)
+    SMIBNetwork(; name, Pmax=1.0e6, delta_grid=0.0, Pdist=0.0)
 
 Classical lossless SMIB power-angle relation:
     Pe = Pmax*sin(delta - delta_grid)
@@ -7,9 +7,9 @@ Classical lossless SMIB power-angle relation:
 @component function SMIBNetwork(; name, Pmax=1.0e6, delta_grid=0.0)
     @named delta = SignalPort()
     @named Pe = SignalPort()
-    @parameters Pmax=Pmax delta_grid=delta_grid
+    @parameters Pmax=Pmax delta_grid=delta_grid Pdist=Pdist
     eqs = [
-        Pe.u ~ Pmax * sin(delta.u - delta_grid)
+        Pe.u ~ Pmax * sin(delta.u - delta_grid) + Pdist
     ]
-    System(eqs, t, [], [Pmax,delta_grid]; systems=[delta,Pe], name=name)
+    System(eqs, t, [], [Pmax,delta_grid,Pdist]; systems=[delta,Pe], name=name)
 end
